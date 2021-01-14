b32cb677d3389ae096f3e4e4a90a12122ebf346b
Hkc-1673 <habibkc71@gmail.com>
2021-01-14 22:35:34 +0200

    Added Header options

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,20 +7,21 @@ function Header() {
             <img className="header__logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" />
             <div className="header__search">
                 <input className="header__searchInput" type="text" name="" />
+            
             </div>
 
             <div className="header__nav">
                 <div className="header__option">
-
+                    <span className="header__optionLineOn">Hello Guest</span>
+                    <span className="header__optionLineTw">Sign In</span>
                 </div>
                 <div className="header__option">
-
+                    <span className="header__optionLineOn">Returns</span>
+                    <span className="header__optionLineTw">& Orders</span>
                 </div>
                 <div className="header__option">
-
-                </div>
-                <div className="header__option">
-                    
+                    <span className="header__optionLineOn">Your</span>
+                    <span className="header__optionLineTw">Prime</span>
                 </div>
             </div>
         </div>
